a4f04b25e64134614e434d717bf11fe5b50e216a
mobber007 <thefactsmob@gmail.com>
2018-08-08 15:41:55 +0300

    Made api_key unsigned

--- database/migrations/2014_05_24_082457_create_api_keys_table.php
+++ database/migrations/2014_05_24_082457_create_api_keys_table.php
@@ -16,7 +16,6 @@ class CreateApiKeysTable extends Migration
             $table->increments('id');
             $table->string('name');
             $table->string('key')->unique('key');
-
             $table->integer('user_id')->unsigned();
             $table->foreign('user_id')->references('id')->on('users');
             $table->timestamps();
